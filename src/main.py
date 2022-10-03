cc5f553af19f20a890fd72cadab0a20ec355e355
Imonikai <108214765+Imonikai@users.noreply.github.com>
2022-10-04 00:28:58 +0900

    firstCommit

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import PySimpleGUI as sg
+import docx
+import os
+
+def convert(from_filename, to_filename):
+    document = docx.Document(from_filename)
+
+    for paragraph in document.paragraphs:
+        paragraph.text = paragraph.text.replace('、','，')
+        paragraph.text = paragraph.text.replace('。','．')
+    
+    document.save(to_filename)
+
+
+layout = [
+    [sg.Text('変換元'), sg.InputText(), sg.FileBrowse(file_types=(('docx','*.docx'),), key='from')],
+    [sg.Text('変換先'), sg.InputText(), sg.FolderBrowse(key='to')],
+    [sg.Submit('変換'), sg.Cancel('やめる')],
+]
+
+window = sg.Window('docxファイルの句読点をカンマピリオドに変換', layout)
+while True:
+    event, values = window.read()
+
+    if( values['from'] != '' and values['to'] != '' and event == '変換'):
+        from_filename = values['from']
+        to_path = values['to']
+        basename = os.path.basename(from_filename)
+        to_filename = f'{to_path}/periodConv-{basename}'
+        convert(from_filename, to_filename)
+        break
+
+    if event in [None, 'やめる']:
+        break
+
+window.close()
